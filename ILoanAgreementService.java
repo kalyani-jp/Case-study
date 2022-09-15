2a9c38f4eff1d1443cb1635adce26d47cebe19ff
kalyani-jp <73516066+kalyani-jp@users.noreply.github.com>
2022-09-15 16:10:50 +0530

    Update ILoanAgreementService.java

--- ILoanAgreementService.java
+++ ILoanAgreementService.java
@@ -3,11 +3,12 @@ package com.homeloanapp.services;
 import java.util.List;   
 
 import com.homeloanapp.entity.LoanAgreement;
+import com.homeloanapp.exception.InvalidLoanAgreementException;
 
 public interface ILoanAgreementService{
 	public LoanAgreement addLoanAgreement(LoanAgreement loanAgreement) ;
-	public LoanAgreement updateLoanAgreement(long loanAgreementId, LoanAgreement loanAgreement);
-	public LoanAgreement deleteLoanAgreement(long loanAgreementId);
-	public LoanAgreement retriveLoanAgreementById(long loanAgreementId);
+	public LoanAgreement updateLoanAgreement(long loanAgreementId, LoanAgreement loanAgreement) throws InvalidLoanAgreementException;;
+	public LoanAgreement deleteLoanAgreement(long loanAgreementId) throws InvalidLoanAgreementException;
+	public LoanAgreement retriveLoanAgreementById(long loanAgreementId) throws InvalidLoanAgreementException;
 	public List<LoanAgreement> retriveAllLoanAgreements();
 }
