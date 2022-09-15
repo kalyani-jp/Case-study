862b938b7c2ecb3abb003ae27fd57f5bf898b481
kalyani-jp <73516066+kalyani-jp@users.noreply.github.com>
2022-09-15 16:12:00 +0530

    Update LoanAgreementService.java

--- LoanAgreementService.java
+++ LoanAgreementService.java
@@ -1,11 +1,14 @@
 package com.homeloanapp.services;
 
-import java.util.List;    
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.homeloanapp.repositories.IHomeLoanAgreementRepository;
 import com.homeloanapp.entity.LoanAgreement;
+import com.homeloanapp.exception.InvalidLoanAgreementException;
 
 @Service
 public class LoanAgreementService implements ILoanAgreementService{
@@ -15,34 +18,31 @@ public class LoanAgreementService implements ILoanAgreementService{
 
 	@Override
 	public LoanAgreement addLoanAgreement(LoanAgreement loanAgreement) {
-		// TODO Auto-generated method stub
 		//LoanAgreement loanAgreement = new LoanAgreement();
 		return loanAgreementRepository.save(loanAgreement);
 	}
 
 	@Override
-	public LoanAgreement updateLoanAgreement(long loanAgreementId, LoanAgreement loanAgreement) {
-		// TODO Auto-generated method stub
-		loanAgreementRepository.findById(loanAgreementId);
-		return loanAgreementRepository.save(loanAgreement);
-	}
+	public LoanAgreement updateLoanAgreement(long loanAgreementId, LoanAgreement loanAgreement)throws InvalidLoanAgreementException {
+		loanAgreementRepository.findById(loanAgreementId).orElseThrow(() -> new InvalidLoanAgreementException("Loan Agreement Not Found!"));
+        return loanAgreementRepository.save(loanAgreement);
+ }
+  
 
 	@Override
-	public LoanAgreement deleteLoanAgreement(long loanAgreementId) {
+	public LoanAgreement deleteLoanAgreement(long loanAgreementId) throws InvalidLoanAgreementException{
 		LoanAgreement loanAgreement = retriveLoanAgreementById(loanAgreementId);
 		loanAgreementRepository.deleteById(loanAgreementId);
 		return loanAgreement;
 	}
 
 	@Override
-	public LoanAgreement retriveLoanAgreementById(long loanAgreementId) {
-		// TODO Auto-generated method stub
-		return loanAgreementRepository.findByLoanAgreementId(loanAgreementId);
+	public LoanAgreement retriveLoanAgreementById(long loanAgreementId)throws InvalidLoanAgreementException {
+		return loanAgreementRepository.findByLoanAgreementId(loanAgreementId).orElseThrow(() -> new InvalidLoanAgreementException("Loan Agreement Not Found!"));
 	}
 
 	@Override
 	public List<LoanAgreement> retriveAllLoanAgreements() {
-		// TODO Auto-generated method stub
 		return loanAgreementRepository.findAll();
 	}
 
